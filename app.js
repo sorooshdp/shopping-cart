44cb9de66f8036975c788592237ed448eaacb3f8
soroosh dadashi <sorooshdpki@gmail.com>
2023-03-27 20:49:23 +0430

    <update> add functions for view class and process shopping cart items

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const productsDOM = document.querySelector('.products')
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('.cart-total');
+const cartItems = document.querySelector('.cart-items')
+const cartTotal = document.querySelector('.cart-total')
 const cartContent = document.querySelector('.cart-content')
+const cartDOM = document.querySelector('.cart')
+const cartOverlay = document.querySelector('.cart-overlay')
+const cartBtn = document.querySelector('.cart-btn')
+const closeCartBtn = document.querySelector('.close-cart')
+const clearCartBtn = document.querySelector('.clear-cart')
 
 let cart = [];
 
@@ -14,11 +19,11 @@ class Product {
             let products = data.items
 
             products = products.map(item => {
-                const {title, price} = item.fields
-                const {id} = item.sys
+                const { title, price } = item.fields
+                const { id } = item.sys
                 const image = item.fields.image.fields.file.url
 
-                return {title, price, id, image}
+                return { title, price, id, image }
             });
 
             return products
@@ -36,13 +41,12 @@ class View {
             <article class="product">
             <div class="img-container">
                 <img src=${item.image} alt=${item.title} class="product-img">
-                <button class="bag-btn" data-id=${item.id} >add</button>
             </div>
             <div class="title-container">
             <h3>${item.title}</h3>
             <h4>price:${item.price}$</h4>
+            <button class="bag-btn" data-id=${item.id} >add</button>
             </div>
-            
         </article>
         `
         });
@@ -50,48 +54,48 @@ class View {
         productsDOM.innerHTML = result
     }
 
-    getCartButtons() {
-        const buttons = [...document.querySelectorAll('.bag-btn')]
+  getCartButtons() {
+    const buttons = [...document.querySelectorAll('.bag-btn')]
 
-        buttons.forEach((item) => {
-            let id = item.dataset.id;
+    buttons.forEach((item) => {
+      let id = item.dataset.id
 
-            item.addEventListener('click', (event) => {
-                let cartItem = {...Storage.getProducts(id), amount: 1}
-                cart = [...cart, cartItem];
-                Storage.saveCart(cart);
+      item.addEventListener('click', (event) => {
+        let cartItem = { ...Storage.getProduct(id), amount: 1 }
+        cart = [...cart, cartItem]
 
-                this.setCarValues(cart)
+        Storage.saveCart(cart)
 
-                this.addCartItem(cartItem)
-            })
-        })
-    }
+        this.setCartValues(cart)
 
-    setCarValues(cart) {
-        let totalPrice = 0, totalItems = 0;
+        this.addCartItem(cartItem)
+      })
+    })
+  }
 
-        cart.map((item) => {
-            totalPrice = totalPrice + item.price * item.amount
-            totalItems = totalItems + item.amount
-        })
+   setCartValues(cart) {
+    let totalPrice = 0
+    let totalItems = 0
 
-        cartTotal.innerHTML = totalPrice;
-        cartItems.innerHTML = totalItems;
+    cart.map((item) => {
+      totalPrice = totalPrice + item.price * item.amount
+      totalItems = totalItems + item.amount
+    })
 
-        console.log(cartTotal, cartItems)
-    }
+    cartTotal.innerText = totalPrice
+    cartItems.innerText = totalItems
+  }
 
-    addCartItem(item) {
-        const div = document.createElement('div')
-        div.classList.add('cart-item')
+   addCartItem(item) {
+    const div = document.createElement('div')
+    div.classList.add('cart-item')
 
-        div.innerHTML = `
+    div.innerHTML = `
     <img src=${item.image} alt=${item.title} />
     <div>
       <h4>${item.title}</h4>
       <h5>${item.price}</h5>
-      <span class="remove-item" data-id=${item.id}>حذف</span>
+      <span class="remove-item" data-id=${item.id}>delete</span>
     </div>
     <div>
       <i class="fas fa-chevron-up" data-id=${item.id}></i>
@@ -99,39 +103,148 @@ class View {
       <i class="fas fa-chevron-down" data-id=${item.id}></i>
     </div>
     `
+    cartContent.appendChild(div)
+  }
 
-        cartContent.appendChild(div);
+   showCart() {
+    cartOverlay.classList.add('transparentBcg')
+    cartDOM.classList.add('showCart')
+  }
 
-        console.log(cartContent)
+  initApp() {
+    cart = Storage.getCart()
+
+    this.setCartValues(cart)
+    this.populate(cart)
+
+    cartBtn.addEventListener('click', this.showCart)
+    closeCartBtn.addEventListener('click', this.hideCart)
+  }
+
+  populate(cart) {
+    cart.forEach((item) => {
+      return this.addCartItem(item)
+    })
+  }
+
+  hideCart() {
+    cartOverlay.classList.remove('transparentBcg')
+    cartDOM.classList.remove('showCart')
+  }
+
+  cartProcess() {
+    clearCartBtn.addEventListener('click', () => {
+      this.clearCart()
+    })
+
+    cartContent.addEventListener('click', (event) => {
+      if (event.target.classList.contains('remove-item')) {
+        let removeItem = event.target
+        let id = removeItem.dataset.id
+
+        cartContent.removeChild(removeItem.parentElement.parentElement)
+
+        this.removeProduct(id)
+      }
+
+      if (event.target.classList.contains('fa-chevron-up')) {
+        let addAmount = event.target
+        let id = addAmount.dataset.id
+
+        let product = cart.find((item) => {
+          return item.id === id
+        })
+
+        product.amount = product.amount + 1
+
+        Storage.saveCart(cart)
+        this.setCartValues(cart)
+
+        addAmount.nextElementSibling.innerText = product.amount
+      }
+
+      if (event.target.classList.contains('fa-chevron-down')) {
+        let lowerAmount = event.target
+        let id = lowerAmount.dataset.id
+
+        let product = cart.find((item) => {
+          return item.id === id
+        })
+
+        product.amount = product.amount - 1
+
+        if (product.amount > 0) {
+          Storage.saveCart(cart)
+          this.setCartValues(cart)
+          lowerAmount.previousElementSibling.innerText = product.amount
+        } else {
+          cartContent.removeChild(lowerAmount.parentElement.parentElement)
+          this.removeProduct(id)
+        }
+      }
+    })
+  }
+
+  clearCart() {
+    let cartItems = cart.map((item) => {
+      return item.id
+    })
+
+    cartItems.forEach((item) => {
+      return this.removeProduct(item)
+    })
+
+    while (cartContent.children.length > 0) {
+      cartContent.removeChild(cartContent.children[0])
     }
+  }
+
+  removeProduct(id) {
+    cart = cart.filter((item) => {
+      return item.id !== id
+    })
+
+    this.setCartValues(cart)
+    Storage.saveCart(cart)
+  }
 }
 
 class Storage {
     static saveProducts(products) {
-        localStorage.setItem('products', JSON.stringify(products))
-    }
+    localStorage.setItem('products', JSON.stringify(products))
+  }
 
-    static saveCart(cart) {
-        localStorage.setItem('cart', JSON.stringify(cart))
-    }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem('products'))
 
-    static getProducts(id) {
-        let products = JSON.parse(localStorage.getItem('products'));
+    return products.find((item) => item.id === id)
+  }
 
-        return products.find((item) => item.id === id)
-    }
+  static saveCart(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }
+
+  static getCart() {
+    return localStorage.getItem('cart')
+      ? JSON.parse(localStorage.getItem('cart'))
+      : []
+  }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const view = new View()
-    const product = new Product()
+document.addEventListener('DOMContentLoaded', () => {
+  const view = new View()
+  const product = new Product()
 
-    product
-        .getProducts()
-        .then((data) => {
-            view.displayProducts(data)
-            Storage.saveProducts(data)
-        }).then(() => {
-        view.getCartButtons()
+  view.initApp()
+
+  product
+    .getProducts()
+    .then((data) => {
+      view.displayProducts(data)
+      Storage.saveProducts(data)
+    })
+    .then(() => {
+      view.getCartButtons()
+      view.cartProcess()
     })
 })
